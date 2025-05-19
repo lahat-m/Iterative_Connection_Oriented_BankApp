ad780c2dfed76151dcdac6b0ab6bb4a1cf5aa0c0
lahat-m <lahatm.apach@gmail.com>
2025-05-19 08:39:30 +0300

    functionality to decode server responses

---
+++ client/client_interpreter.h
@@ -0,0 +1,14 @@
+/*
+ * Banking System - INterpreter Module
+ */
+#include "bank_client.h"
+
+#ifndef CLIENT_INTERPRETER
+#define CLIENT_INTERPRETER
+
+
+
+/* Server function prototypes */
+void interpret_response(response_t *response, command_t command, int account_number);
+
+#endif /* CLIENT_INTERPRETER */
